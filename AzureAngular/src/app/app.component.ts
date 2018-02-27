0e23ef9c1aede6e8557e7c589981bbb1fe89d2d1
RaphaelFonseca <raphael@fonseca.tech>
2018-02-27 08:46:59 -0500

    finshed simple chat for angular 5

--- AzureAngular/src/app/app.component.ts
+++ AzureAngular/src/app/app.component.ts
@@ -1,45 +1,38 @@
-import {
-  Component
-} from '@angular/core';
-import {
-  HubConnection
-} from '@aspnet/signalr-client';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Azure-SignalR';
-  private hubConnection: HubConnection;
+export class AppComponent implements OnInit {
+  private _hubConnection: HubConnection;
   nick = '';
   message = '';
   messages: string[] = [];
 
+  public sendMessage(): void {
+    this._hubConnection
+      .invoke('sendToAll', this.nick, this.message)
+      .then(() => this.message = '')
+      .catch(err => console.error(err));
+  }
 
   ngOnInit() {
     this.nick = window.prompt('Your name:', 'John');
 
-    this.hubConnection = new HubConnection('http://localhost:5000/chat');
+    this._hubConnection = new HubConnection('http://localhost:5000/chat');
 
-    this.hubConnection
+    this._hubConnection
       .start()
-      .then(() => console.log('Connection Started:'))
+      .then(() => console.log('Connection started!'))
       .catch(err => console.log('Error while establishing connection :('));
 
-    this.hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
-      const text = `${nick}: ${receivedMessage}`;
-      this.messages.push(text);
-    });
-  }
+      this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+        const text = `${nick}: ${receivedMessage}`;
+        this.messages.push(text);
+      });
 
-  // Send message to client from TO server
-  public sendMessage(): void {
-    this.hubConnection
-      .invoke('sendToAll', this.nick, this.message)
-      .catch(err => console.error(err));
-  }
-
-
+    }
 }
